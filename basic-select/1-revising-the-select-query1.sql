060bc2e9f6cbd66c311e927e28dfc76ea98aadeb
Chien-Ming Huang <cmhuang0328@gmail.com>
2017-11-29 23:26:21 -0500

    Update 1-revising-the-select-query1.sql

--- basic-select/1-revising-the-select-query1.sql
+++ basic-select/1-revising-the-select-query1.sql
@@ -1,4 +1,5 @@
 # Query all columns for all American cities in CITY with populations larger than 100000. The CountryCode for America is USA.
+
 # Input Format
 # The CITY table is described as follows:
 # CITY (Field : Type)
@@ -7,5 +8,6 @@
 # COUNTRYCODE : VARCHAR2(3)
 # DISTRICT : VARCHAR2(20)
 # POPULATION : NUMBER
+
 SELECT * FROM city c
 WHERE c. population > 100000 AND c.countrycode = 'USA';
