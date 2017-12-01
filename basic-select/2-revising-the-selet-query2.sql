045889a5fce5f3e120f3c2133454be835eb30e0f
Chien-Ming Huang <cmhuang0328@gmail.com>
2017-11-30 23:50:49 -0500

    Update 2-revising-the-selet-query2.sql

--- basic-select/2-revising-the-selet-query2.sql
+++ basic-select/2-revising-the-selet-query2.sql
@@ -1,13 +1,15 @@
-# Query the names of all American cities in CITY with populations larger than 120000. The CountryCode for America is USA
+/* Query the names of all American cities in CITY with populations larger than 120000. The CountryCode for America is USA */
 
-# Input Format
-# The CITY table is described as follows:
-# CITY (Field : Type)
-# ID : NUMBER
-# NAME : VARCHAR2(17)
-# COUNTRYCODE : VARCHAR2(3)
-# DISTRICT : VARCHAR2(20)
-# POPULATION : NUMBER
+/*
+Input Format
+The CITY table is described as follows:
+CITY (Field : Type)
+ID : NUMBER
+NAME : VARCHAR2(17)
+COUNTRYCODE : VARCHAR2(3)
+DISTRICT : VARCHAR2(20)
+POPULATION : NUMBER
+*/
 
 SELECT c.name FROM city c
 WHERE c.population >120000 AND c.countrycode = 'USA';
