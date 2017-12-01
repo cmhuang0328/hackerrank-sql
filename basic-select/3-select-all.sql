7fbbd8957e36759617eb1295be2486e92b38c60f
Chien-Ming Huang <cmhuang0328@gmail.com>
2017-11-30 23:51:24 -0500

    Update 3-select-all.sql

--- basic-select/3-select-all.sql
+++ basic-select/3-select-all.sql
@@ -1,12 +1,14 @@
-# Query all columns (attributes) for every row in the CITY table
+/* Query all columns (attributes) for every row in the CITY table */
 
-# Input Format
-# The CITY table is described as follows:
-# CITY (Field : Type)
-# ID : NUMBER
-# NAME : VARCHAR2(17)
-# COUNTRYCODE : VARCHAR2(3)
-# DISTRICT : VARCHAR2(20)
-# POPULATION : NUMBER
+/*
+Input Format
+The CITY table is described as follows:
+CITY (Field : Type)
+ID : NUMBER
+NAME : VARCHAR2(17)
+COUNTRYCODE : VARCHAR2(3)
+DISTRICT : VARCHAR2(20)
+POPULATION : NUMBER
+*/
 
 SELECT * FROM city;
