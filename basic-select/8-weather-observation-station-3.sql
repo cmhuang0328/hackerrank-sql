58f7ceeb9f13f9a86e86a79f8f674b9ecdc172a8
Chien-Ming Huang <cmhuang0328@gmail.com>
2017-11-30 23:57:14 -0500

    Create 8-weather-observation-station-3.sql

---
+++ basic-select/8-weather-observation-station-3.sql
@@ -0,0 +1,16 @@
+/* Query a list of CITY names from STATION with even ID numbers only. You may print the results in any order, but must exclude duplicates from your answer */
+
+/*
+Input Format
+The STATION table is described as follows:
+STATION (Field : Type)
+ID : NUMBER
+CITY : VARCHAR2(21)
+STATE : VARCHAR2(2)
+LAT_N : NUMBER
+LONG_W : NUMBER
+where LAT_N is the northern latitude and LONG_W is the western longitude
+*/
+
+SELECT DISTINCT(s.city) FROM station s
+WHERE  mod(s.id, 2) = 0;
