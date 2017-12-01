3f2ed9b3c2a59b9a6933aab4f01c5c76fb283c59
Chien-Ming Huang <cmhuang0328@gmail.com>
2017-11-30 23:52:20 -0500

    Update 5-japanese-cities-attributes.sql

--- basic-select/5-japanese-cities-attributes.sql
+++ basic-select/5-japanese-cities-attributes.sql
@@ -1,13 +1,15 @@
-# Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN
+/* Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN */
 
-# Input Format
-# The CITY table is described as follows:
-# CITY (Field : Type)
-# ID : NUMBER
-# NAME : VARCHAR2(17)
-# COUNTRYCODE : VARCHAR2(3)
-# DISTRICT : VARCHAR2(20)
-# POPULATION : NUMBER
+/*
+Input Format
+The CITY table is described as follows:
+CITY (Field : Type)
+ID : NUMBER
+NAME : VARCHAR2(17)
+COUNTRYCODE : VARCHAR2(3)
+DISTRICT : VARCHAR2(20)
+POPULATION : NUMBER
+*/
 
 SELECT * FROM city c
 WHERE c.countrycode = 'JPN';
