355d178c815183970e73b54eb0bff2530049c147
Chien-Ming Huang <cmhuang0328@gmail.com>
2017-11-30 23:49:28 -0500

    Update 7-weather-observation-station-1.sql

--- basic-select/7-weather-observation-station-1.sql
+++ basic-select/7-weather-observation-station-1.sql
@@ -1,12 +1,15 @@
-# Query a list of CITY and STATE from the STATION table
+/* Query a list of CITY and STATE from the STATION table */
 
-# Input Format
-# The STATION table is described as follows:
-# STATION (Field : Type)
-# ID : NUMBER
-# CITY : VARCHAR2(21)
-# STATE : VARCHAR2(2)
-# LAT_N : NUMBER
-# LONG_W : NUMBER
+/*
+Input Format
+The STATION table is described as follows:
+STATION (Field : Type)
+ID : NUMBER
+CITY : VARCHAR2(21)
+STATE : VARCHAR2(2)
+LAT_N : NUMBER
+LONG_W : NUMBER
+where LAT_N is the northern latitude and LONG_W is the western longitude
+*/
 
 SELECT s.city, s.state FROM station s;
