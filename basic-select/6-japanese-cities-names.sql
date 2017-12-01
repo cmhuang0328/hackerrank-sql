582e538ef84cac93a011845778da6ab8ee2e03c2
Chien-Ming Huang <cmhuang0328@gmail.com>
2017-11-30 23:52:51 -0500

    Update 6-japanese-cities-names.sql

--- basic-select/6-japanese-cities-names.sql
+++ basic-select/6-japanese-cities-names.sql
@@ -1,13 +1,15 @@
-# Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN
+/* Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN */
 
-# Input Format
-# The CITY table is described as follows:
-# CITY (Field : Type)
-# ID : NUMBER
-# NAME : VARCHAR2(17)
-# COUNTRYCODE : VARCHAR2(3)
-# DISTRICT : VARCHAR2(20)
-# POPULATION : NUMBER
+/*
+Input Format
+The CITY table is described as follows:
+CITY (Field : Type)
+ID : NUMBER
+NAME : VARCHAR2(17)
+COUNTRYCODE : VARCHAR2(3)
+DISTRICT : VARCHAR2(20)
+POPULATION : NUMBER
+*/
 
 SELECT c.name FROM city c
 WHERE c.countrycode = 'JPN';
