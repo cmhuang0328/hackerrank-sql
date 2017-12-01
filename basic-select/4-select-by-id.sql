922fbe7d63a949019b16631efcc021704eac0d17
Chien-Ming Huang <cmhuang0328@gmail.com>
2017-11-30 23:51:50 -0500

    Update 4-select-by-id.sql

--- basic-select/4-select-by-id.sql
+++ basic-select/4-select-by-id.sql
@@ -1,13 +1,15 @@
-# Query all columns for a city in CITY with the ID 1661
+/* Query all columns for a city in CITY with the ID 1661 */
 
-# Input Format
-# The CITY table is described as follows:
-# CITY (Field : Type)
-# ID : NUMBER
-# NAME : VARCHAR2(17)
-# COUNTRYCODE : VARCHAR2(3)
-# DISTRICT : VARCHAR2(20)
-# POPULATION : NUMBER
+/*
+Input Format
+The CITY table is described as follows:
+CITY (Field : Type)
+ID : NUMBER
+NAME : VARCHAR2(17)
+COUNTRYCODE : VARCHAR2(3)
+DISTRICT : VARCHAR2(20)
+POPULATION : NUMBER
+*/
 
 SELECT * FROM city c
 WHERE c.id = 1661;
