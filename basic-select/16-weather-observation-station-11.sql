c4f9a88a40aa9236011b3092079a0639bc717fb8
Chien-Ming Huang <cmhuang0328@gmail.com>
2017-12-01 19:47:56 -0500

    Create 16-weather-observation-station-11.sql

---
+++ basic-select/16-weather-observation-station-11.sql
@@ -0,0 +1,19 @@
+/*
+Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. 
+Your result cannot contain duplicates.
+*/
+
+/*
+Input Format
+The STATION table is described as follows:
+STATION (Field : Type)
+ID : NUMBER
+CITY : VARCHAR2(21)
+STATE : VARCHAR2(2)
+LAT_N : NUMBER
+LONG_W : NUMBER
+where LAT_N is the northern latitude and LONG_W is the western longitude
+*/
+
+SELECT distinct s.city FROM station s
+WHERE s.city not REGEXP '^[aeiou]' or s.city not REGEXP '[aeiou]$';
