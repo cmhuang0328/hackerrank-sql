c7295ef5236abc19107e5ee03c03269a6e3d32f2
Chien-Ming Huang <cmhuang0328@gmail.com>
2017-12-08 23:55:23 -0500

    Create 7-the-blunder.sql

---
+++ aggregation/7-the-blunder.sql
@@ -0,0 +1,20 @@
+/*
+Samantha was tasked with calculating the average monthly salaries for all employees in the EMPLOYEES table, 
+but did not realize her keyboard's  key was broken until after completing the calculation. 
+She wants your help finding the difference between her miscalculation (using salaries with any zeroes removed),
+and the actual average salary.
+
+Write a query calculating the amount of error (i.e.: actual-miscalculated average monthly salaries), and round it up to the next integer.
+*/
+
+/*
+Input Format
+The EMPLOYEES table is described as follows:
+Employee (Field : Type)
+ID: Integer
+Name: String
+Salary: Integer
+*/
+
+SELECT (ROUND(AVG(A.Salary))-ROUND(AVG(REPLACE(A.Salary, "0", "")))) AS Error
+FROM EMPLOYEES A;
