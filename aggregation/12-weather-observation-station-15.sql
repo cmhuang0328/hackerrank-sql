f9668cb34e3095beda1f25abec1736f2c83efe0e
Chien-Ming Huang <cmhuang0328@gmail.com>
2017-12-12 10:32:48 -0500

    Create 12-weather-observation-station-15.sql

---
+++ aggregation/12-weather-observation-station-15.sql
@@ -0,0 +1,19 @@
+/* 
+Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137.2345. 
+Round your answer to 4 decimal places.
+*/
+
+/*
+Input Format
+The STATION table is described as follows:
+STATION (Field : Type)
+ID : NUMBER
+CITY : VARCHAR2(21)
+STATE : VARCHAR2(2)
+LAT_N : NUMBER
+LONG_W : NUMBER
+where LAT_N is the northern latitude and LONG_W is the western longitude
+*/
+
+SELECT round(s.long_w, 4) FROM station s
+WHERE s.lat_n = (SELECT max(a.lat_n) FROM station a WHERE a.lat_n < 137.2345);
