adbabf4ce71c0d4ac0f00acbfb88af64f7089c3f
Chien-Ming Huang <cmhuang0328@gmail.com>
2017-12-11 23:47:09 -0500

    Create 9-weather-observation-station-2.sql

---
+++ aggregation/9-weather-observation-station-2.sql
@@ -0,0 +1,19 @@
+/* 
+Query the following two values from the STATION table:
+The sum of all values in LAT_N rounded to a scale of 2 decimal places.
+The sum of all values in LONG_W rounded to a scale of 2 decimal places.
+*/
+
+/*
+Input Format
+The STATION table is described as follows:
+STATION (Field : Type)
+ID : NUMBER
+CITY : VARCHAR2(21)
+STATE : VARCHAR2(2)
+LAT_N : NUMBER
+LONG_W : NUMBER
+where LAT_N is the northern latitude and LONG_W is the western longitude
+*/
+
+SELECT round(sum(s.lat_n), 2), round(sum(s.long_w), 2) FROM station s;
