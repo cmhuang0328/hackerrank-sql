041e42de0f4ce2cfd04cf145904cbeba0877f192
Chien-Ming Huang <cmhuang0328@gmail.com>
2018-01-10 11:18:46 -0500

    Create 2-african-cities.sql

---
+++ basic-join/2-african-cities.sql
@@ -0,0 +1,36 @@
+/*
+Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+*/
+
+/*
+Input Format
+The CITY and COUNTRY tables are described as follows:
+CITY (Field : Type)
+ID: NUMBER
+NAME: VARCHAR2(17)
+COUNTRYCODE: VARCHAR2(3)
+DISTRICT: VARCHAR2(20)
+POPULATION: NUMBER
+COUNTRY (Field : Type)
+CODE: VARCHAR2(3)
+NAME: VARCHAR2(44)
+CONTINENT: VARCHAR2(13)
+REGION: VARCHAR2(25)
+SURFACEAREA: NUMBER
+INDEPYEAR: VARCHAR2(5)
+POPULATION: NUMBER
+LIFEEXPECTANCY: VARCHAR2(4)
+GNP: NUMBER
+GNPOLD: VARCHAR2(9)
+LOCALNAME: VARCHAR2(44)
+GOVERNMENTFORM: VARCHAR2(44)
+HEADOFSTATE: VARCHAR2(32)
+CAPITAL: VARCHAR2(4)
+CODE: VARCHAR2(2)
+*/
+
+/* Use FROM table 1 INNER JOIN table 2 ON to deal with this
+SELECT a.name FROM city a
+INNER JOIN country b ON a.countrycode = b.code
+WHERE b.continent = 'Africa';
